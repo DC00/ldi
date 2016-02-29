3e5f2e274e83b6f1e8f05afbc998935b23147271
raymondzhao <raymondfuzhao@gmail.com>
2016-02-28 21:17:06 -0500

    changed main.py

--- typechecker/main.py
+++ typechecker/main.py
@@ -12,10 +12,20 @@ ast_lines = ast_filehandler.readlines()
 ast_filehandler.close()
 
 # Keep track of classes in a list (alphabetically)
+	# *There should always be 6 classes: Main, Object, IO, Int, String, Bool
 
+class_list = ['Main','Object','IO','Int','String','Bool']
 
+	# Find all classes (they are capitalized)
+
+for line in ast_lines:
+	if line.isupper() and line not in class_list:
+		class_list.append(line)
+	
 
 # Keep track of attributes from the classes 
+
+
 	
 	# Are they initialized?
 
