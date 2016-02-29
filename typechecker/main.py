88ce83268e47116e2fd00a0d69800ba5acf37592
raymondzhao <raymondfuzhao@gmail.com>
2016-02-28 20:21:14 -0500

    adding necessary files for typechecker

---
+++ typechecker/main.py
@@ -0,0 +1,23 @@
+# Raymond Zhao rfz5nt
+# Daniel Coo djc4ku
+# PA4 - Type Checker
+
+import sys
+
+# Get input from parse tree
+
+ast_file = sys.argv[1]
+ast_filehandler = open(ast_file, 'r')
+ast_lines = ast_filehandler.readlines()
+ast_filehandler.close()
+
+# Keep track of classes in a list (alphabetically)
+
+
+
+# Keep track of attributes from the classes 
+	
+	# Are they initialized?
+
+# Output the class_map
+
