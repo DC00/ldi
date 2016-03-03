12f439c85d7cd40f0b48e47ae348b66b81b9f645
DC00 <djc4ku@virginia.edu>
2016-03-03 17:19:22 -0500

    new testing script

--- typechecker/assmap.sh
+++ typechecker/assmap.sh
@@ -4,7 +4,8 @@ TEST_FILE="$1"
 if [[ -e main.ml ]] ; then
   cool --class-map $TEST_FILE --out ref_${TEST_FILE%%.*}
   ocamlopt -o main.exe main.ml
-  ./main.exe $TEST_FILE
+  cool --parse $TEST_FILE
+  ./main.exe $TEST_FILE-ast
   diff -b -B -E -w ref_$TEST_FILE-type $TEST_FILE-type
   vimdiff $TEST_FILE-type ref_$TEST_FILE-type
 fi 
