35da54e7023e96660fe940c13c67076b71bffbe4
DC00 <djc4ku@virginia.edu>
2016-03-31 02:55:17 -0400

    ruby

--- interpreter/pa5c/main.rb
+++ interpreter/pa5c/main.rb
@@ -1,1 +1,14 @@
-# 
+# Raymond Zhao (rfz5nt)
+# Daniel Coo (djc4ku)
+# PA5C
+
+# Read in .cl-type file
+# Separate into class_map, imp_map, parent_map, and the annotated AST lists
+
+cl_type_file = ARGV[0]
+
+File.foreach( cl_type_file ) do |line|
+	line.chomp!
+	puts line
+end
+
