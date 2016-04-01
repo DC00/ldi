2582f410cd2a14c2ba5777df1f613b5465cd317f
DC00 <djc4ku@virginia.edu>
2016-04-01 02:33:07 -0400

    reading in lists

--- interpreter/pa5c/main.rb
+++ interpreter/pa5c/main.rb
@@ -7,8 +7,50 @@
 
 cl_type_file = ARGV[0]
 
+# Read in .cl-type file
+type_file = []
 File.foreach( cl_type_file ) do |line|
 	line.chomp!
-	puts line
+	type_file << line
 end
+
+# Break into separate maps
+class_map = []
+imp_map = []
+parent_map = []
+ast = []
+
+type_file.each do |line|
+	if line == "implementation_map"
+		break
+	end
+	class_map << line
+end
+
+type_file = type_file - class_map
+
+type_file.each do |line|
+	if line == "parent_map"
+		break
+	end
+	imp_map << line
+end
+
+type_file
+# 2.times { parent_map << type_file.shift }
+
+class_map.each do |l|
+	puts l
+end
+
+puts
+puts
+
+imp_map.each do |l|
+	puts l
+end
+
+
+
+
 
